5bf072576c4ad3d557eac9158a674edc61b2f810
caprico613 <tom871226@gmail.com>
2020-04-01 17:46:32 +0800

    add

--- uLCD/main.cpp
+++ uLCD/main.cpp
@@ -2,23 +2,46 @@
 
 #include "uLCD_4DGL.h"
 
-
+PwmOut PWM1(D7);
 uLCD_4DGL uLCD(D1, D0, D2);
+Serial pc( USBTX, USBRX );
+//AnalogIn Ain(A0);
+DigitalIn  ADCdata(D7);
 
+float j;
 
 int main()
 
 {
 
     uLCD.locate(5, 0);
-      uLCD.printf("\n106061245\n"); //Default Green on black text
-      uLCD.line(20, 20, 20, 60, BLACK);
+    uLCD.printf("\n106061245\n"); //Default Green on black text
+    uLCD.line(20, 20, 20, 60, BLACK);
     uLCD.line(20, 20, 60, 20, BLACK);
     uLCD.line(20, 60, 60, 60, BLACK);   // x,y just like cooridinate plane
     uLCD.line(60, 20, 60, 60, BLACK);
-      //uLCD.circle(64 , 64 , 64, BLACK); // total width and length are both 128
-      uLCD.background_color(WHITE);  // white background
-      wait(30);
+    uLCD.background_color(WHITE);  // white background
 
+    while(1){
+
+        PWM1.period(0.001);
+        for (float i = 0; i <= 1; i+=0.1)
+        {
+            PWM1 = i;
+            j = ADCdata;
+            pc.printf("%1.3f\r\n", j);           
+            wait(0.1);
+        }
+        
+        for (float i = 1; i >= 0; i-=0.1)
+        {
+            PWM1 = i;
+             j = ADCdata;
+            pc.printf("%1.3f\r\n", j);    
+            wait(0.1);
+        }       
+
+
+    }
 
 }
